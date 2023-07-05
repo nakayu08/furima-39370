878db130468669525b797895c038af968a13a730
nakayu08 <nakabo.yu08@gmail.com>
2023-07-05 19:17:09 +0900

    配送料の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :delivery_charge
+  
 
 
   belongs_to :user
@@ -12,4 +14,6 @@ class Item < ApplicationRecord
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
+
 end
