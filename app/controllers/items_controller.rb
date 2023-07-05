368b58053d7a1d46f8e2e19128d7577ca202b71f
nakayu08 <nakabo.yu08@gmail.com>
2023-07-05 15:55:12 +0900

    必要なコントローラーの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id,:condition_id,:delivery_charge_id,:prefecture_id,:shipment_id,:price,:user)
+  end
+
 end
