d56f79846a8120dc48c4907f40f99c8dede3b752
nakayu08 <nakabo.yu08@gmail.com>
2023-07-05 19:31:07 +0900

    発送までの日数の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :delivery_charge
   belongs_to :prefecture
+  belongs_to :shipment
   
 
 
@@ -12,10 +13,12 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :explanation, presence: true
+  validates :price, presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipment_id, numericality: { other_than: 1, message: "can't be blank" } 
 
 end
