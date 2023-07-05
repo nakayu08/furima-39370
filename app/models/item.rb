9997b19d630640d2e9469786a2b587e728ce5779
nakayu08 <nakabo.yu08@gmail.com>
2023-07-05 19:12:40 +0900

    商品の状態の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
 
 
   belongs_to :user
@@ -10,4 +11,5 @@ class Item < ApplicationRecord
   validates :explanation, presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
