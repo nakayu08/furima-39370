86820abe447dd20d9abf46c4f6d72df4dbc254c5
nakayu08 <nakabo.yu08@gmail.com>
2023-07-05 19:27:25 +0900

    都道府県の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_charge
+  belongs_to :prefecture
   
 
 
@@ -15,5 +16,6 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
 
 end
