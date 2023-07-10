9446dc64ccafd49f88c916db4546fd9dcdd67396
nakayu08 <nakabo.yu08@gmail.com>
2023-07-10 15:18:31 +0900

    ビューファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item =Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id,:condition_id,:delivery_charge_id,:prefecture_id,:shipment_id,:price,:image).merge(user_id: current_user.id)
