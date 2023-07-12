3f7672a10396f53f5d6132b813f4ba7bb7ad8482
nakayu08 <nakabo.yu08@gmail.com>
2023-07-12 18:20:13 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,22 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @order_from =OrderFrom.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_from =OrderFrom.new(order_params)
     if @order_from.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_from).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number,:item_id).merge(user_id: current_user.id)
+    params.require(:order_from).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id,item_id: @item.id)
   end
+end
