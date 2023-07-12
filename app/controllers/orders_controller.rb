3a931d8a02a757c11bb93467adda7f6370693838
nakayu08 <nakabo.yu08@gmail.com>
2023-07-12 17:06:00 +0900

    formオブジェクトの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,19 @@
 class OrdersController < ApplicationController
   def index
+    @order_from =OrderFrom.new
   end
 
   def create
+    @order_from =OrderFrom.new(order_params)
+    if @order_from.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
-end
+
+  private
+
+  def order_params
+    params.require(:order_from).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number,:item_id).merge(user_id: current_user.id)
+  end
--- app/models/order_from.rb
+++ app/models/order_from.rb
@@ -1,10 +1,23 @@
 class OrderFrom
   include ActiveModel::Model
-  attr_accessor :
+  attr_accessor :post_code,:prefecture_id,:municipalities,:address,:building_name,:telephone_number,:order_id,:user_id,:item_id
 
   # ここにバリデーションの処理を書く
+  validates :post_code, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :municipalities, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :order_id, presence: true
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+
+  
 
   def save
     # 各テーブルにデータを保存する処理を書く
+    order =Order.create(:user_id user_id,:item_id item_id)
+
+    Address.create(:post_code post_code,:prefecture_id prefecture_id,:municipalities municipalities,:address address,:building_name building_name,:telephone_number telephone_number,:order_id order.id)
   end
 end
