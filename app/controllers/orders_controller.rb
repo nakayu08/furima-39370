566c94c4829dfea594de659fb0c564d3b1cd0d35
nakayu08 <nakabo.yu08@gmail.com>
2023-07-19 19:16:23 +0900

    ログイン状態の場合でも、自身が出品した商品の商品購入ページに遷移しようとすると、商品の販売状況に関わらずトップページに遷移すること。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!,only:[:index]
+
   def index
     @item = Item.find(params[:item_id])
     @order_from =OrderFrom.new
+
+    if @item.user_id == current_user.id || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
