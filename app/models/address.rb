2cbab5603490fd44557705e1e53c0b59db05b8d8
nakayu08 <nakabo.yu08@gmail.com>
2023-08-08 17:49:07 +0900

    エラーメッセの日本語化

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください" } 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
